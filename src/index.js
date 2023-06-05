fe9a61d26b2e2eeee911ae1f999281a53978879d
Shourya742 <84662239+Shourya742@users.noreply.github.com>
2023-06-05 10:51:42 +0530

    Added create ticket api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
-const mailsender = require("./config/email-config");
 
 const app = express();
 app.use(express.json());
@@ -11,15 +10,4 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-  //   try {
-  //     const response = await mailsender.sendMail({
-  //       from: ServerConfig.GMAIL_EMAIL,
-  //       to: "abs@gmail.com",
-  //       subject: "Sab theek",
-  //       text: "Bas chal raha hai",
-  //     });
-  //     console.log(response);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
 });
