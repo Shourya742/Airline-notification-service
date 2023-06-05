ec8a13b1a00457b9c57e21e0026005dba1ca4fef
Shourya742 <84662239+Shourya742@users.noreply.github.com>
2023-06-05 12:27:51 +0530

    completed consumption and sending mail

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ const express = require("express");
 
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
+const amqplib = require("amqplib");
+const { EmailService } = require("./services");
+async function connectQueue() {
+  try {
+    const connection = await amqplib.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+    await channel.assertQueue("noti-queue");
+    channel.consume("noti-queue", async (data) => {
+      const obj = JSON.parse(`${Buffer.from(data.content)}`);
+      await EmailService.sendEmail(
+        "abc@gmail.com",
+        obj.recepientEmail,
+        obj.subject,
+        obj.text
+      );
+      channel.ack(data);
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
 const app = express();
 app.use(express.json());
